d301e255f3b75da7f57c612717863999129f049c
William Howenstine <williamhowenstine@Williams-MacBook-Air.local>
2014-06-18 11:39:38 -0400

    fixed to make object orienteted and simplified play method by abstracting methods

--- lottery.rb
+++ lottery.rb
@@ -1,13 +1,32 @@
-names = []
-loop do
-  puts "please enter names: "
-  entries = gets.chomp
-  names.push entries
-  if entries == ""
-    break
+class Lottery
+  def initialize
+    @names = []
+  end
+  def play
+    print_instructions
+    get_names
+    print_winner
+  end
+
+  private
+
+  def print_instructions
+    puts "Enter in a list of names followed by a blank line to sart the lottery"
+  end
+  def get_names
+   loop do 
+     print "> "
+     name = gets.chomp
+     if name.empty?
+       break
+     end
+     @names << name  #pushes entry in names array
+   end
+  end
+  def print_winner
+   puts "#{@names.sample} wins!"  #sample will just pick a random value
   end
 end
-winner = names.shuffle.sample
 
-puts "Congrat's! " + winner.to_s + " is the winner!" 
-
+lottery = Lottery.new
+lottery.play
