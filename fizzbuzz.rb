2a690a44d5fd9074056898dcc5dea2f15e787366
William Howenstine <williamhowenstine@Williams-MacBook-Air.local>
2014-06-18 16:10:10 -0400

    simplified code a little

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,14 +3,14 @@ class FizzBuzz
     i=0
     while i < 100
         i += 1
-      if i.modulo(3).zero? && i.modulo(5).zero?
+      if i.modulo(15).zero?
         puts "FizzBuzz"
       elsif i.modulo(3).zero?
         puts "Fizz"
       elsif i % 5 ==  0
         puts "Buzz"
       else 
-        puts i.to_s
+        puts i
       end
     end
   end
