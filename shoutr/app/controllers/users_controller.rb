f35b302a870527ad89a051b8d5a677ed39f68d73
wowenstine <whowenstine@gmail.com>
2014-07-14 10:27:33 -0400

    intial commit ran monban scaffold auto genereate user auth

---
+++ shoutr/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  #in monban there is an auto requirement that users be logged in so in this case need to specify skip login
+  skip_before_action :require_login, only: [:new, :create]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = sign_up(user_params)
+
+    if @user.valid?
+      sign_in(@user)
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
+
